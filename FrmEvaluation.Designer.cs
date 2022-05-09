0b3a675757ba8f66af6948f05f5a2a3f17eb7547
PI <pi_foi@outlook.com>
2022-05-09 13:32:05 +0200

    skoro dovršena treća vježba, implementacija evaluiranja studenta

--- FrmEvaluation.Designer.cs
+++ FrmEvaluation.Designer.cs
@@ -48,9 +48,6 @@
 			// 
 			// label1
 			// 
-			this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.label1.Location = new System.Drawing.Point(12, 9);
@@ -58,13 +55,9 @@
 			this.label1.Size = new System.Drawing.Size(64, 16);
 			this.label1.TabIndex = 0;
 			this.label1.Text = "Aktivnost:";
-			this.label1.Click += new System.EventHandler(this.label1_Click);
 			// 
 			// cboActivities
 			// 
-			this.cboActivities.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.cboActivities.FormattingEnabled = true;
 			this.cboActivities.Location = new System.Drawing.Point(12, 28);
 			this.cboActivities.Name = "cboActivities";
@@ -73,9 +66,6 @@
 			// 
 			// txtMinForSignature
 			// 
-			this.txtMinForSignature.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.txtMinForSignature.Location = new System.Drawing.Point(432, 29);
 			this.txtMinForSignature.Name = "txtMinForSignature";
 			this.txtMinForSignature.Size = new System.Drawing.Size(130, 20);
@@ -83,9 +73,6 @@
 			// 
 			// label2
 			// 
-			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.label2.AutoSize = true;
 			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.label2.Location = new System.Drawing.Point(449, 10);
@@ -96,9 +83,6 @@
 			// 
 			// label3
 			// 
-			this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.label3.AutoSize = true;
 			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.label3.Location = new System.Drawing.Point(655, 11);
@@ -109,9 +93,6 @@
 			// 
 			// txtMinForGrade
 			// 
-			this.txtMinForGrade.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.txtMinForGrade.Location = new System.Drawing.Point(641, 29);
 			this.txtMinForGrade.Name = "txtMinForGrade";
 			this.txtMinForGrade.Size = new System.Drawing.Size(135, 20);
@@ -119,9 +100,6 @@
 			// 
 			// txtTeacher
 			// 
-			this.txtTeacher.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.txtTeacher.Location = new System.Drawing.Point(437, 336);
 			this.txtTeacher.Name = "txtTeacher";
 			this.txtTeacher.Size = new System.Drawing.Size(100, 20);
@@ -129,9 +107,6 @@
 			// 
 			// txtEvaluatioDate
 			// 
-			this.txtEvaluatioDate.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.txtEvaluatioDate.Location = new System.Drawing.Point(555, 336);
 			this.txtEvaluatioDate.Name = "txtEvaluatioDate";
 			this.txtEvaluatioDate.Size = new System.Drawing.Size(122, 20);
@@ -139,9 +114,6 @@
 			// 
 			// numPoints
 			// 
-			this.numPoints.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.numPoints.Location = new System.Drawing.Point(690, 336);
 			this.numPoints.Name = "numPoints";
 			this.numPoints.Size = new System.Drawing.Size(80, 20);
@@ -150,9 +122,6 @@
 			// 
 			// label4
 			// 
-			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.label4.AutoSize = true;
 			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.label4.Location = new System.Drawing.Point(434, 316);
@@ -163,9 +132,6 @@
 			// 
 			// label5
 			// 
-			this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.label5.AutoSize = true;
 			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.label5.Location = new System.Drawing.Point(552, 316);
@@ -176,9 +142,6 @@
 			// 
 			// label6
 			// 
-			this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.label6.AutoSize = true;
 			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.label6.Location = new System.Drawing.Point(687, 317);
@@ -189,9 +152,6 @@
 			// 
 			// btnSave
 			// 
-			this.btnSave.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.btnSave.Location = new System.Drawing.Point(591, 396);
 			this.btnSave.Name = "btnSave";
@@ -202,9 +162,6 @@
 			// 
 			// btnCancel
 			// 
-			this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
 			this.btnCancel.Location = new System.Drawing.Point(690, 396);
 			this.btnCancel.Name = "btnCancel";
@@ -212,12 +169,10 @@
 			this.btnCancel.TabIndex = 14;
 			this.btnCancel.Text = "Odustani";
 			this.btnCancel.UseVisualStyleBackColor = true;
+			this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
 			// 
 			// textBox1
 			// 
-			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.textBox1.Location = new System.Drawing.Point(12, 55);
 			this.textBox1.Multiline = true;
 			this.textBox1.Name = "textBox1";
--- FrmEvaluation.cs
+++ FrmEvaluation.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Evaluation_Manager.Models;
+using Evaluation_Manager.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,19 +14,27 @@ namespace Evaluation_Manager
 {
 	public partial class FrmEvaluation : Form
 	{
-		public FrmEvaluation()
+		private Student odabraniStudent;
+		public FrmEvaluation(Student odabraniStudent)
 		{
 			InitializeComponent();
-		}
-
-		private void label1_Click(object sender, EventArgs e)
-		{
-
+			this.odabraniStudent = odabraniStudent;
 		}
 
 		private void FrmEvaluation_Load(object sender, EventArgs e)
 		{
+			SetFormText();
+			var activities = ActivityRepository.GetActivities();
+			cboActivities.DataSource = activities;
+		}
+		private void SetFormText()
+		{
+			Text = odabraniStudent.FirstName + " " + odabraniStudent.LastName;
+		}
 
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			Close();
 		}
 	}
 }
